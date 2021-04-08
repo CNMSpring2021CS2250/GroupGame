0e0494bd9c66120bed483b587ea83c33c51c59a1
Chadfire <chadfire10@gmail.com>
2021-04-08 07:20:39 -0600

    Created Pause Menu Script

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Author  : Stephen Murchison
+ * Email   : smurchison1@cnm.edu
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creates the ability to hide and unhide a gameobject.
+/// </summary>
+public class PauseMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Causes the pause menu to be visible
+    /// </summary>
+    public void Pause()
+        => gameObject.SetActive(true);
+
+    /// <summary>
+    /// Deactivates the pause menu
+    /// </summary>
+    public void Play()
+        => gameObject.SetActive(false);
+}
