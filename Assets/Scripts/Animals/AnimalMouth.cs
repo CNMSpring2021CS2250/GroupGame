680c16c2f508d8b5febcc7bf9d363fa39a46b647
Chadfire <chadfire10@gmail.com>
2021-04-16 20:29:34 -0600

    Added comments

--- Assets/Scripts/Animals/AnimalMouth.cs
+++ Assets/Scripts/Animals/AnimalMouth.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/**
+ * Author  : Stephen Murchison
+ * Email   : smurchison1@cnm.edu
+ */
 using UnityEngine;
 
+/// <summary>
+/// Provides the animal a way to eat and interact with food.
+/// Tells the animal if they are touching a berry with their mouth.
+/// </summary>
 public class AnimalMouth : MonoBehaviour
 {
 
+    /// <summary>
+    /// Is the mouth collider touching a berry
+    /// </summary>
     public bool TouchingFood { get; private set; }
 
     private void OnTriggerEnter(Collider other)
