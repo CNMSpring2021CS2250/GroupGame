6e1c36bfd3983d5363ec00ca1f459e7a83d8af72
Chadfire <chadfire10@gmail.com>
2021-04-20 19:12:05 -0600

    Got ticket sound to work

---
+++ Assets/Scripts/Architecture/TicketBooth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketBooth : MonoBehaviour
+{
+
+    public AudioClip[] ticketSounds;
+
+    private AudioSource ticketBoothAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ticketBoothAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ticketBoothAudioSource.PlayOneShot(ticketSounds[Random.Range(0, ticketSounds.Length)]);
+        }
+    }
+}
