ac35e34e8d1fb1944bf6742b6433f207086a7bc1
Chadfire <chadfire10@gmail.com>
2021-04-21 07:24:46 -0600

    Created sound scripts

---
+++ Assets/Scripts/Animals/AnimalSounder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AnimalSounder : MonoBehaviour
+{
+
+    public AudioClip AudioClip;
+
+    private AudioSource AudioSource;
+
+    void Start()
+    {
+        GetComponent<AudioSource>();
+        StartCoroutine(MakeAnimalSounds());
+    }
+
+    private IEnumerator MakeAnimalSounds()
+    {
+        if (AudioClip != null)
+        {
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(Random.Range(5, 15));
+                AudioSource.PlayOneShot(AudioClip);
+            }
+        }
+    }
+}
